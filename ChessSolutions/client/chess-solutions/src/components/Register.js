b9552c1c92544731c0b9384a73d83368912b9e6d
Detective-Coder <aaronfrankenfield@gmail.com>
2021-06-28 11:37:39 -0400

    finished register file

--- ChessSolutions/client/chess-solutions/src/components/Register.js
+++ ChessSolutions/client/chess-solutions/src/components/Register.js
@@ -32,6 +32,29 @@ export default function Register() {
           <Label htmlFor="firstName">First Name</Label>
           <Input id="firstName" type="text" onChange={e => setFirstName(e.target.value)} />
         </FormGroup>
+        <FormGroup>
+          <Label htmlFor="lastName">Last Name</Label>
+          <Input id="lastName" type="text" onChange={e => setLastName(e.target.value)} />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="userName">Username</Label>
+          <Input id="userName" type="text" onChange={e => setUserName(e.target.value)} />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="text" onChange={e => setEmail(e.target.value)} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" onChange={e => setPassword(e.target.value)} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="confirmPassword">Confirm Password</Label>
+          <Input id="confirmPassword" type="password" onChange={e => setConfirmPassword(e.target.value)} />
+        </FormGroup>
+        <FormGroup>
+          <Button>Register</Button>
+        </FormGroup>
       </fieldset>
     </Form>
   )

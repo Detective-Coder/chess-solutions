05ada959a7fbcd9d99fdcb5b4d44946626d5dd78
Detective-Coder <aaronfrankenfield@gmail.com>
2021-06-29 09:38:23 -0400

    installing more serverside packages

--- ChessSolutions/Utils/DbUtils.cs
+++ ChessSolutions/Utils/DbUtils.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
 
 namespace ChessSolutions.Utils
 {

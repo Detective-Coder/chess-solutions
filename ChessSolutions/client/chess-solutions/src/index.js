b23fd12cf8f63d08e1699fb078b693a0117a9d5a
Detective-Coder <aaronfrankenfield@gmail.com>
2021-06-29 10:30:36 -0400

    updated appsettings.json with firebase id

--- ChessSolutions/client/chess-solutions/src/index.js
+++ ChessSolutions/client/chess-solutions/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +23,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

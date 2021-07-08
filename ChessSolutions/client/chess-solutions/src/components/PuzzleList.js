80c35c67811920cc053332deadadf76ff6e77f97
Detective-Coder <aaronfrankenfield@gmail.com>
2021-07-08 16:00:48 -0400

    working on reactstrap

--- ChessSolutions/client/chess-solutions/src/components/PuzzleList.js
+++ ChessSolutions/client/chess-solutions/src/components/PuzzleList.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { PuzzleContext } from "../providers/PuzzleProvider";
 import Puzzle from "./Puzzle";
 import { Link } from 'react-router-dom';
+import { Button, Container, Row, Col } from 'reactstrap';
 
 const PuzzleList = () => {
   const { puzzles, getAllPuzzles } = useContext(PuzzleContext);
@@ -11,25 +12,32 @@ const PuzzleList = () => {
   }, []);
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="cards-column">
-          <h2>Beginner Puzzles</h2>
-          <p>1 - 10</p>
-          <h2>Intermediate Puzzles</h2>
-          <p>11 - 20</p>
-          <h2>Advanced Puzzles</h2>
-          <p>21 - 30</p>
-          {puzzles.map((puzzle) => (
-            <>
-              <Puzzle key={puzzle.id} puzzle={puzzle} />
-              <Link to={`/puzzle/${puzzle.id}/solutions`}>View Puzzle</Link>
-            </>
-          ))}
-        </div>
-      </div>
+    // <div className="container">
+    //   <div className="row justify-content-center">
+    //     <div className="cards-column">
+    //       <h2>Beginner Puzzles</h2>
+    //       <p>1 - 10</p>
+    //       <h2>Intermediate Puzzles</h2>
+    //       <p>11 - 20</p>
+    //       <h2>Advanced Puzzles</h2>
+    //       <p>21 - 30</p>
+    //       {puzzles.map((puzzle) => (
+    //         <>
+    //           <Puzzle key={puzzle.id} puzzle={puzzle} />
+    //           <Button color="primary">
+    //             <Link to={`/puzzle/${puzzle.id}/solutions`} className="text-white">View Puzzle</Link>
+    //           </Button>
+    //         </>
+    //       ))}
+    //     </div>
+    //   </div>
 
-    </div>
+    // </div>
+    <Container>
+      <Row>
+        
+      </Row>
+    </Container>
   );
 };
 

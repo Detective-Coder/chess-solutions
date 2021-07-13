f4abeac3cd2c6c66a4c7195c49a4625b518ade54
Detective-Coder <aaronfrankenfield@gmail.com>
2021-07-13 11:53:41 -0400

    styled login page

--- ChessSolutions/client/chess-solutions/src/components/Login.js
+++ ChessSolutions/client/chess-solutions/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { useHistory, Link } from "react-router-dom";
 import { UserProfileContext } from "../providers/UserProfileProvider";
+import { Container, Row, Col } from 'reactstrap';
 
 export default function Login() {
   const history = useHistory();
@@ -19,22 +20,26 @@ export default function Login() {
 
   return (
     <Form onSubmit={loginSubmit}>
-      <fieldset>
-        <FormGroup>
-          <Label for="email">Email</Label>
-          <Input id="email" type="text" onChange={e => setEmail(e.target.value)} />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input id="password" type="password" onChange={e => setPassword(e.target.value)} />
-        </FormGroup>
-        <FormGroup>
-          <Button>Login</Button>
-        </FormGroup>
-        <em>
-          Not registered? <Link to="register">Register</Link>
-        </em>
-      </fieldset>
+      <Row>
+        <Col xs="4" className="mx-auto">
+          <fieldset className="mt-5">
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input id="email" type="text" onChange={e => setEmail(e.target.value)} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input id="password" type="password" onChange={e => setPassword(e.target.value)} />
+            </FormGroup>
+            <FormGroup>
+              <Button color="primary">Login</Button>
+            </FormGroup>
+            <em>
+              Not registered? <Link to="register">Register</Link>
+            </em>
+          </fieldset>
+        </Col>
+      </Row>
     </Form>
   );
 }
